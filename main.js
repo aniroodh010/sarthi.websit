54bfa80c2a2ce359091198ed97d54cc42d0c0e9b
KumarNithish12 <64510519+KumarNithish12@users.noreply.github.com>
2023-08-16 22:52:08 +0530

    Update sign in url

--- main.js
+++ main.js
@@ -51,5 +51,5 @@ sections.forEach((section) => {
 const authButton = document.getElementById('authButton');
 
 authButton.addEventListener('click', () => {
-  window.open('https://google.com', '_blank');
+  window.open('https://sarthii.auth.ap-south-1.amazoncognito.com/login?client_id=4ka7je6v48ddsbl8j9b5b080fq&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fec2-3-6-90-191.ap-south-1.compute.amazonaws.com', '_blank');
 });

f529b3a97964b29aaff26da28cacb3991ce7ebc3
kumarnithish12 <kumarnithish1999@gmail.com>
2023-08-27 21:09:44 +0530

    changed all - nk

--- main.js
+++ main.js
@@ -53,3 +53,34 @@ const authButton = document.getElementById('authButton');
 authButton.addEventListener('click', () => {
   window.open('https://sarthii.auth.ap-south-1.amazoncognito.com/login?client_id=4ka7je6v48ddsbl8j9b5b080fq&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fec2-3-6-90-191.ap-south-1.compute.amazonaws.com', '_blank');
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const contactForm = document.getElementById('contact-form');
+  contactForm.addEventListener('submit', function (event) {
+      event.preventDefault(); // Prevents the default form submission
+      
+      const formData = new FormData(contactForm);
+      const url = 'https://r2q0wd3p94.execute-api.ap-south-1.amazonaws.com/sarthi/upload';
+  
+      fetch(url, {
+          method: 'POST',
+          body: JSON.stringify(Object.fromEntries(formData)),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      })
+      .then(response => response.json())
+      .then(data => {
+          alert(data); // Display success message
+          contactForm.reset(); // Clear form fields
+      })
+      .catch(error => {
+          alert("Error. Please try again"); // Display error message
+      });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.style.opacity = 1; // Show content after everything is loaded
+});
